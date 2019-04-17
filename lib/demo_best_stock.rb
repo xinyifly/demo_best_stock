0528e60a80490395758fdc2e0b1b73b246de5efa
Zeyu Chen <xinyifly@live.cn>
2019-04-17 21:03:25 +0800

    feat: DemoBestStock add best

--- lib/demo_best_stock.rb
+++ lib/demo_best_stock.rb
@@ -1,4 +1,28 @@
+require 'set'
+
 class DemoBestStock
+  def self.best(items)
+    stocks = {}
+    items.each do |item|
+      Float(item['Value']) rescue next
+      stocks.merge!(
+        item['Name'] => SortedSet.new([RecordByDate.from_plain(item)]),
+      ) { |key, oldset, newset| oldset + newset }
+    end
+
+    best_stock = stocks.reduce(nil) do |stock, (key, set)|
+      array = set.to_a
+      increment = array.last.Value - array.first.Value
+
+      if stock.nil? || increment > stock['Increment']
+        { 'Name' => key, 'Increment' => increment }
+      else
+        stock
+      end
+    end
+    best_stock['Increment'] = best_stock['Increment'].to_s('F')
+    best_stock
+  end
 end
 
 require 'demo_best_stock/record'
--- test/test_demo_best_stock.rb
+++ test/test_demo_best_stock.rb
@@ -1,5 +1,37 @@
 require 'minitest/autorun'
 require 'demo_best_stock'
+require 'csv'
 
 class DemoBestStockTest < Minitest::Test
+  def test_best
+    items = CSV.parse(<<~ROWS, headers: true)
+      Name,Date,Value
+      c,2015-1-1,0.5
+      d,2015-1-10,7
+      c,2015-1-5,10.5
+      c,2015-1-2,5.5
+      d,2015-1-12,37
+      d,2015-1-22,2
+      c,2015-1-1,0.5
+    ROWS
+
+    assert_equal(
+      { 'Name' => 'c', 'Increment' => '10.0' },
+      DemoBestStock.best(items)
+    )
+  end
+
+  def test_best_bad_input
+    items = CSV.parse(<<~ROWS, headers: true)
+      Name,Date,Value
+      c,2015-1-1,0.5
+      c,2015-1-2,10.5
+      c,2015-1-7,N/A
+    ROWS
+
+    assert_equal(
+      { 'Name' => 'c', 'Increment' => '10.0' },
+      DemoBestStock.best(items)
+    )
+  end
 end
